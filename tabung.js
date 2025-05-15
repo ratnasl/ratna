542e2e6026121ccbb972b3a9a89364a48d8547fd
ratnasl <ratnasulistia29@gmail.com>
2025-05-15 22:37:10 +0700

    Tugas 7

---
+++ tabung.js
@@ -0,0 +1,10 @@
+//  Menghitung Luas Permukaan Tabung
+let T = 20; // tinggi tabung
+let r = 4; // jari-jari tabung
+const pi = 3.14;
+
+// Menghitung luas permukaan tabung
+let luas = 2 * pi * r * (r + T);
+
+// Menampilkan hasil
+console.log(luas);
