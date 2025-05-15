0f42d6e98b9898a858cad69d16170255453fb59a
ratnasl <ratnasulistia29@gmail.com>
2025-05-15 22:36:40 +0700

    Tugas 7

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+// String Concatenation
+let firstName = 'Ratna';
+let lastName = 'Zendrato';
+let bornYear = 2006;
+
+let currentYear = new Date().getFullYear();
+let age = currentYear - bornYear;
+
+// Menampilkan hasil ke console
+console.log(`Hello, ${firstName} ${lastName}! You are ${age} years old.`);
