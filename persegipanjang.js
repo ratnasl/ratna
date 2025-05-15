0af64278e7f96d146b5e9b5f1d67884417f08c5c
ratnasl <ratnasulistia29@gmail.com>
2025-05-15 22:37:50 +0700

    Tugas 7

---
+++ persegipanjang.js
@@ -0,0 +1,11 @@
+// Deklarasi variabel panjang dan lebar
+let panjang = 20;
+let lebar = 5;
+
+// Menghitung luas dan keliling persegi panjang
+let luas = panjang * lebar;
+let keliling = 2 * (panjang + lebar);
+
+// Menampilkan hasil ke console
+console.log(`Luas persegi panjang = ${luas}`);
+console.log(`Keliling persegi panjang = ${keliling}`);
